27e192be3c93f300b8a10415b62e3f2a4f514721
Jerin Kebha <kebhajerin@gmail.com>
2023-04-08 19:25:36 +1000

    updated stripe public key

--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -7,7 +7,9 @@ import { useAppDispatch } from "../../app/store/configureStore";
 import { setBasket } from "../basket/basketSlice";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 
-const stripePromise = loadStripe('pk_test_51JV6ejEiX6mroRZ6LZfplGw2mBnYSq6Jvj0T927DOTLH7nqQbSKXYDmuB1RsaVPitHEJEYo1cz5Dd8Naagiaxe5X00OqnaFoUY');
+const stripePromise = loadStripe(
+    "pk_test_51MnS9oHrMvXvvE6lXYH7z09tJqgglWjyM5hswGepBcGGCmtbtke8C0UGnQodnw6ubEGhZkemnzgsNaZ5HlCmecBE00pFsdsupv"
+);
 
 export default function CheckoutWrapper() {
     const dispatch = useAppDispatch();
@@ -15,16 +17,16 @@ export default function CheckoutWrapper() {
 
     useEffect(() => {
         agent.Payments.createPaymentIntent()
-            .then(response => dispatch(setBasket(response)))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
+            .then((response) => dispatch(setBasket(response)))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
     }, [dispatch]);
 
-    if (loading) return <LoadingComponent message='Loading checkout' />
+    if (loading) return <LoadingComponent message="Loading checkout" />;
 
     return (
         <Elements stripe={stripePromise}>
             <CheckoutPage />
         </Elements>
-    )
-} 
+    );
+}
